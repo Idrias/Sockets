fc267ab23590779785cdba7b9b233b7c4c62e274
Idrias <nomail1@gmx.net>
2018-10-17 11:16:04 +0200

    added udp exercise

---
+++ src/d_ProtocolUDP/UDP_Client.java
@@ -0,0 +1,50 @@
+package d_ProtocolUDP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+public class UDP_Client {
+
+	public static int port = 4998;
+	
+	public static void main(String[] args) {
+		
+		// Open UDP socket
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		
+		
+		while(true) {
+			
+			// Get user input
+			String input = null;
+			try {
+				System.out.print(">");
+				input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			// Send message as broadcast
+			byte[] bytes = input.getBytes();
+			try {
+				System.out.println(bytes + " " + bytes.length);
+				socket.send(new DatagramPacket(bytes, bytes.length, new InetSocketAddress("255.255.255.255", port)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+}
---
+++ src/d_ProtocolUDP/UDP_Server.java
@@ -0,0 +1,45 @@
+package d_ProtocolUDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UDP_Server {
+
+	public static int port = 4998;
+	public static int buffSize = 1024;
+	
+	public static void main(String[] args) {
+		
+		// Open UDP socket
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		
+		while(true) {
+		try {
+			
+				// Receive datagram packet 
+				DatagramPacket dp = new DatagramPacket(new byte[buffSize], buffSize);
+				socket.receive(dp);
+				
+				// Print received message
+				String message = new String(dp.getData());
+				System.out.println("Got report from " + dp.getAddress() + ": " + message);
+				
+			} catch (SocketException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}	
+		
+	}
+	
+}
